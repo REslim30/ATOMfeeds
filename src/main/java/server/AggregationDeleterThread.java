31d79591f63a7fd6503024fcecfee205a116fe14
Aidan Do <giahuydo99@gmail.com>
2020-09-14 07:58:04 +0930

    Deleted unused boolean and StorageManager delete implemeneted

---
+++ src/main/java/server/AggregationDeleterThread.java
@@ -0,0 +1,16 @@
+package main.java.server;
+
+/**
+ * AggregationDeleterThread
+ */
+public class AggregationDeleterThread extends Thread {
+    private AggregationStorageManager storage;
+
+    public AggregationDeleterThread(AggregationStorageManager storage) {
+        this.storage = storage;
+    }
+
+    public void run() {
+        
+    }
+}
