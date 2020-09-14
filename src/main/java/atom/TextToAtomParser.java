895c07028a90ba2669e3febdd08e832c0f04ab42
Aidan Do <giahuydo99@gmail.com>
2020-09-14 21:43:30 +0930

    TextToAtomParserTests.java

--- src/main/java/atom/TextToAtomParser.java
+++ src/main/java/atom/TextToAtomParser.java
@@ -25,10 +25,12 @@ import java.io.*;
 public class TextToAtomParser {
     BufferedReader in;
     StringBuilder atomBuilder;
+    boolean hasFeedAuthor;
 
     public TextToAtomParser(BufferedReader in) {
         this.in = in;
         atomBuilder = new StringBuilder();
+        hasFeedAuthor = false;
     }
 
     //Parses the input stream into atom
@@ -46,6 +48,7 @@ public class TextToAtomParser {
         boolean hasTitle = false;
         boolean hasId = false;
         boolean hasUpdated = false;
+        boolean hasSubtitle = false;
 
         String line;
         String[] keyValuePair = new String[2];
@@ -63,26 +66,37 @@ public class TextToAtomParser {
                     hasTitle = true;
                     parseTextElement("title", keyValuePair[1]);
                     break;
+
                 case "updated":
                     if (hasUpdated)
                         throw new IOException("RFC-4287: A feed MUST only contain one updated element.");
                     hasUpdated = true;
                     parseTextElement("updated", keyValuePair[1]);
                     break;
+
                 case "id":
                     if (hasId)
                         throw new IOException("RFC-4287: A feed MUST only contain one id element.");
+                    hasId = true;
                     parseTextElement("id", keyValuePair[1]);
                     break;
+
                 case "subtitle":
+                    if (hasSubtitle)
+                        throw new IOException("RFC-4287: A feed MUST contain at most one subtitle.");
+                    hasSubtitle = true;
                     parseTextElement("subtitle", keyValuePair[1]);
                     break;
+
                 case "author":
+                    hasFeedAuthor = true;
                     parseAuthor(keyValuePair[1]);
                     break;
+
                 case "link":
                     parseLink(keyValuePair[1]);
                     break;
+
                 default:
                     throw new IOException("Unknown atom element: " + keyValuePair[0]);
             }
@@ -91,14 +105,100 @@ public class TextToAtomParser {
 
         if (line != null) {
             if (line.equals("entry")) {
-                //parseEntry()
+                parseEntry();
             } else {
                 throw new IOException("Unknown text element: " + line);
             }
         }
+
+        if (!hasTitle | !hasUpdated | !hasId) 
+           throw new IOException("RFC-4287: Missing title, updated or id element"); 
         
         atomBuilder.append("</feed>");
     }
+
+    
+    //Parses a atom:entry
+    //Calls itself recursively on multiple entries
+    private void parseEntry() throws IOException {
+        atomBuilder.append("<entry>");
+
+        boolean hasAuthor = false;
+        boolean hasId = false;
+        boolean hasTitle = false;
+        boolean hasUpdated = false;
+        boolean hasSummary = false;
+
+        String line;
+        String[] keyValuePair = new String[2];
+        while ((line = in.readLine()) != null) {
+            int delim = line.indexOf(':');
+            if (delim == -1)
+                break;
+
+            keyValuePair[0] = line.substring(0, delim);
+            keyValuePair[1] = line.substring(delim+1);
+
+            switch (keyValuePair[0]) {
+                case "author":
+                    hasAuthor = true;
+                    parseAuthor(keyValuePair[1]);
+                    break;
+
+                case "id":
+                    if (hasId)
+                        throw new IOException("RFC-4287: atom:entry must contain exactly one id");
+                    hasId = true;
+                    parseTextElement("id", keyValuePair[1]);
+                    break;
+
+                case "link":
+                    parseLink(keyValuePair[1]);
+                    break;
+
+                case "title":
+                    if (hasTitle)
+                        throw new IOException("RFC-4287: atom:entry must contain exactly one title");
+                    hasTitle = true;
+                    parseTextElement("title", keyValuePair[1]);
+                    break;
+
+                case "updated":
+                    if (hasUpdated)
+                        throw new IOException("RFC-4287: atom:entry must contain exactly one updated element");
+                    hasUpdated = true;
+                    parseTextElement("updated", keyValuePair[1]);
+                    break;
+
+                case "summary":
+                    if (hasSummary)
+                        throw new IOException("RFC-4287: atom:entry must contain at most one summary");
+                    hasSummary = true;
+                    parseTextElement("summary", keyValuePair[1]);
+                    break;
+
+                default:
+                    throw new IOException("Unknown text element: " + keyValuePair[0]);
+            }
+        }
+
+        if (!hasFeedAuthor && !hasAuthor)
+            throw new IOException("RFC-4287: atom:entry must contain an author if atom:feed does not contain an author.");
+
+        if (!hasId || !hasTitle || !hasUpdated)
+            throw new IOException("RFC-4286: atom:entry is missing an id, title or updated element");
+
+        atomBuilder.append("</entry>");
+        if (line != null) {
+            if (line.equals("entry")) {
+                parseEntry();
+                return;            
+            } else {
+                throw new IOException("Unknown text element: " + line);
+            }
+        }
+    }
+
 
     //Parses an atom:author 
     private void parseAuthor(String value) {
@@ -112,6 +212,7 @@ public class TextToAtomParser {
         atomBuilder.append("<link href=\"" + value + "\">");
         atomBuilder.append("</link>");
     }
+
 
     //Generic Parser
     //Parses a type="text" atom element
--- src/test/java/atom/TextToAtomParserTest.java
+++ src/test/java/atom/TextToAtomParserTest.java
@@ -8,6 +8,11 @@ import org.junit.Test;
 
 /**
  * TextToAtomParserTest
+ * Tests for the TextToAtomParser object.
+ * Note: A tradeoff was made between using String.contains
+ * and strict equals. Using String.contains means that
+ * there is a chance of a false positive however it gives 
+ * room for implementation changes.
  */
 public class TextToAtomParserTest {
 
@@ -22,10 +27,50 @@ public class TextToAtomParserTest {
 
     @Test(expected = IOException.class)
     public void throwsExceptionOnNoTitle() throws IOException {
-        String result = readFile("no_title.txt");
+        readFile("no_title.txt");
     }
 
-    //TODO: Test for every possible character
+    @Test(expected = IOException.class)
+    public void throwsExceptionOnNoUpdated() throws IOException {
+        readFile("no_updated.txt");
+    }
+
+    @Test(expected = IOException.class)
+    public void throwsExceptionOnNoId() throws IOException {
+        readFile("no_id.txt");
+    }
+
+    @Test(expected = IOException.class)
+    public void throwsExceptionOnDuplicateTitle() throws IOException {
+        readFile("duplicate_title.txt");
+    }
+
+    @Test(expected = IOException.class)
+    public void throwsExceptionOnDuplicateId() throws IOException {
+        readFile("duplicate_id.txt");
+    }
+
+    @Test(expected = IOException.class)
+    public void throwsExceptionOnDuplicateUpdated() throws IOException {
+        readFile("duplicate_updated.txt");
+    }
+
+    @Test(expected = IOException.class)
+    public void throwsExceptionOnDuplicateSubtitle() throws IOException {
+        readFile("duplicate_subtitle.txt");
+    }
+
+    @Test
+    public void parsesSpecialCharacters() throws IOException {
+        String result = readFile("special_characters.txt");
+        assertEquals("author",true, result.contains("<author><name><>?:\"{}}+_)(*&^%$#@!~`[]\\;',./</name></author>"));
+        assertEquals("title",true, result.contains("<title><>?:\"{}}+_)(*&^%$#@!~`[]\\;',./</title>"));
+        assertEquals("updated",true, result.contains("<updated><>?:\"{}}+_)(*&^%$#@!~`[]\\;',./</updated>"));
+        assertEquals("id",true, result.contains("<id><>?:\"{}}+_)(*&^%$#@!~`[]\\;',./</id>"));
+        assertEquals("subtitle",true, result.contains("<subtitle><>?:\"{}}+_)(*&^%$#@!~`[]\\;',./</subtitle>"));
+        assertEquals("link",true, result.contains("<link href=\"<>?:\"{}}+_)(*&^%$#@!~`[]\\;',./\"></link>"));
+    }
+
 
     @Test
     public void parsesLink() throws IOException {
@@ -35,6 +80,93 @@ public class TextToAtomParserTest {
         assertEquals("id", true, result.contains("<id>5%412934871239</id>"));
         assertEquals("link", true, result.contains("<link href=\"http://starling.us/cgi-bin/gus_atom_xsl.pl?atom=starling_us_atom.xml!sort_order=Category\"></link>"));
         assertEquals("author", true, result.contains("<author><name>baker's delight</name></author>"));
+    }
+
+    // ***Entry testing***
+    @Test
+    public void parsesSingleEntry() throws IOException {
+        String result = readFile("entry.txt");
+        assertEquals("title", true, result.contains("<title>entry title</title>"));
+        assertEquals("updated", true, result.contains("<updated>2007-03-25T03:49:00Z</updated>"));
+        assertEquals("id", true, result.contains("<id>sdflakjdf</id>"));
+        assertEquals("entry", true, result.contains("<entry>") && result.contains("</entry>"));
+    }
+
+    @Test
+    public void parsesThreeEntries() throws IOException {
+        String result = readFile("3_entries.txt");
+        assertEquals("1-title", true, result.contains("<title>first entry</title>"));
+        assertEquals("1-updated", true, result.contains("<updated>2007-03-25T03:49:00Z</updated>"));
+        assertEquals("1-id", true, result.contains("<id>different entry</id>"));
+        assertEquals("1-entry", true, result.contains("<entry>") && result.contains("</entry>"));
+
+        assertEquals("2-title", true, result.contains("<title>second entry</title>"));
+        assertEquals("2-updated", true, result.contains("<updated>2007-20-25T03:49:00Z</updated>"));
+        assertEquals("2-id", true, result.contains("<id>876</id>"));
+
+        assertEquals("3-title", true, result.contains("<title>third entry</title>"));
+        assertEquals("3-updated", true, result.contains("<updated>2007-30-25T03:49:00Z</updated>"));
+        assertEquals("3-id", true, result.contains("<id>456</id>"));
+    }
+
+    @Test
+    public void allEntryElementsWithSpecialCharacters() throws IOException {
+        String result = readFile("entry_all_elements.txt");
+
+        assertEquals("title", true, result.contains("<title><?>?:\"{}|_)(*&^%$#@!C~`,./;'[]\\</title>"));
+        assertEquals("updated", true, result.contains("<updated>2007-20-25T03:49:00Z<?>?:\"{}|_)(*&^%$#@!C~`,./;'[]\\</updated>"));
+        assertEquals("id", true, result.contains("<id>876<?>?:\"{}|_)(*&^%$#@!C~`,./;'[]\\</id>"));
+        assertEquals("author1", true, result.contains("<author><name>Steve hooligan<?>?:\"{}|_)(*&^%$#@!C~`,./;'[]\\</name></author>"));
+        assertEquals("author2", true, result.contains("<author><name>different hooligan<?>?:\"{}|_)(*&^%$#@!C~`,./;'[]\\</name></author>"));
+        assertEquals("summary", true, result.contains("<summary>Here's a useful summary kjp<?>?:\"{}|_)(*&^%$#@!C~`,./;'[]\\</summary>"));
+        assertEquals("link1", true, result.contains("<link href=\"<?>?:\"{}|_)(*&^%$#@!C~`,./;'[]\\\"></link>"));
+        assertEquals("link2", true, result.contains("<link href=\"Different Link<?>?:\"{}|_)(*&^%$#@!C~`,./;'[]\\\"></link>"));
+
+    }
+
+    @Test(expected = IOException.class)
+    public void throwsExceptionOnDuplicateEntryId() throws IOException {
+        readFile("entry_duplicate_id.txt");
+    }
+
+    @Test(expected = IOException.class)
+    public void throwsExceptionOnDuplicateEntryTitle() throws IOException {
+        readFile("entry_duplicate_title.txt");
+    }
+
+    @Test(expected = IOException.class)
+    public void throwsExceptionOnDuplicateEntryUpdated() throws IOException {
+        readFile("entry_duplicate_updated.txt");
+    }
+
+    @Test(expected = IOException.class)
+    public void throwsExceptionOnDuplicateEntrySummary() throws IOException {
+        readFile("entry_duplicate_summary.txt");
+    }
+
+    @Test(expected = IOException.class)
+    public void throwsNoExceptionOnNoAuthorWhenFeedNoAuthor() throws IOException {
+        readFile("entry_no_author.txt");
+    }
+
+    @Test
+    public void throwsNoExceptionOnAllAuthorWhenFeedNoAuthor() throws IOException {
+        readFile("entry_all_authors.txt");
+    }
+
+    @Test(expected = IOException.class)
+    public void throwsExceptionOnEntryNoId() throws IOException {
+        readFile("entry_no_id.txt");
+    }
+
+    @Test(expected = IOException.class)
+    public void throwsExceptionOnEntryNoUpdated() throws IOException {
+        readFile("entry_no_updated.txt");
+    }
+
+    @Test(expected = IOException.class)
+    public void throwsExceptionOnEntryNoTitle() throws IOException {
+        readFile("entry_no_title.txt");
     }
 
 
