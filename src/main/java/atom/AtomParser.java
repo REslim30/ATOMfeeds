5eec9fc6dc941f300e42b7ba210b56fb51df6c8e
Aidan Do <giahuydo99@gmail.com>
2020-09-15 14:59:31 +0930

    AtomParser implementation + tests completed

--- src/main/java/atom/AtomParser.java
+++ src/main/java/atom/AtomParser.java
@@ -15,6 +15,8 @@ import org.xml.sax.SAXException;
 public class AtomParser {
     DocumentBuilder builder;
     InputStream input;
+    Document doc;
+    boolean hasFeedAuthor = false;
 
     public AtomParser(String body) {
         try {
@@ -30,7 +32,7 @@ public class AtomParser {
     }
 
     public void parseAtom() throws IOException, SAXException, InvalidAtomException {
-        Document doc = builder.parse(input);
+        doc = builder.parse(input);
         doc.getDocumentElement().normalize();
 
         verifyAtom(doc);
@@ -43,6 +45,9 @@ public class AtomParser {
 
         //Verifies feed
         boolean hasTitle = false;
+        boolean hasId = false;
+        boolean hasUpdated = false;
+        boolean hasSubtitle = false;
         NodeList children = root.getChildNodes();
         for (int i=0; i<children.getLength(); i++) {
             Node child = children.item(i);
@@ -52,10 +57,122 @@ public class AtomParser {
                         throw new InvalidAtomException("Feed element must only contain exactly one title.");
                     hasTitle = true;
                     break;
+                case "id":
+                    if (hasId)
+                        throw new InvalidAtomException("Feed element must only contain exactly one id.");
+                    hasId = true;
+                    break;
+                case "updated":
+                    if (hasUpdated)
+                        throw new InvalidAtomException("Feed element must only contain exactly one updated element.");
+                    hasUpdated = true;
+                    break;
+                case "subtitle":
+                    if (hasSubtitle)
+                        throw new InvalidAtomException("Feed element must have at most one subtitle.");
+                    hasSubtitle = true;
+                    break;
+                case "#text":
+                    break;
+                case "link":
+                    break;
+                case "author":
+                    hasFeedAuthor = true;
+                    verifyAuthor(child);
+                    break;
+                case "entry":
+                    verifyEntry(child);
+                    break;
+                default:
+                    throw new InvalidAtomException("Unknown Feed Element: " + child.getNodeName());
             }
         }
 
-        if (!hasTitle)
-            throw new InvalidAtomException("Feed element is missing title.");
+        if (!hasTitle | !hasId | !hasUpdated)
+            throw new InvalidAtomException("Feed element is missing title, id.");
+
+    }
+
+    private void verifyEntry(Node entry) throws InvalidAtomException {
+        NodeList childNodes = entry.getChildNodes();
+
+        boolean hasTitle = false;
+        boolean hasId = false;
+        boolean hasUpdated = false;
+        boolean hasSummary = false;
+        boolean hasAuthor = false;
+        for (int i=0; i<childNodes.getLength(); i++) {
+            Node child = childNodes.item(i);
+            switch (child.getNodeName()) {
+                case "#text":
+                    break;
+                case "link":
+                    break;
+                case "title":
+                    if (hasTitle)
+                        throw new InvalidAtomException("Entry must only have one title.");
+                    hasTitle = true;
+                    break;
+                case "id":
+                    if (hasId)
+                        throw new InvalidAtomException("Entry must only have one Id.");
+                    hasId = true;
+                    break;
+                case "updated":
+                    if (hasUpdated)
+                        throw new InvalidAtomException("Entry must only have one updated element.");
+                    hasUpdated = true;
+                    break;
+                case "summary":
+                    if (hasSummary)
+                        throw new InvalidAtomException("Entry must only have at most summary element.");
+                    hasSummary = true;
+                    break;
+                case "author":
+                    hasAuthor = true;
+                    verifyAuthor(child);
+                    break;
+            }
+        }
+
+        if (!hasTitle | !hasId | !hasUpdated) 
+            throw new InvalidAtomException("Entry is missing title, id.");
+
+        if (!hasFeedAuthor && !hasAuthor) 
+            throw new InvalidAtomException("Entry must have author if feed does not have author.");
+    }
+
+    //Checks that Author's have the right elements
+    private void verifyAuthor(Node author) throws InvalidAtomException {
+        NodeList childNodes = author.getChildNodes();
+
+        boolean hasName = false;
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node child = childNodes.item(i);
+            switch(child.getNodeName()) {
+                case "name":
+                    hasName = true;
+                    break;
+                case "#text":
+                    break;
+                default:
+                    throw new InvalidAtomException("Unknown Author element: " + child.getNodeName());
+            }
+        }
+        if (!hasName)
+            throw new InvalidAtomException("Author must have a name");
+    }
+
+    //Gets a representation of the Feed
+    //Returns null if Document is unitilized
+    //Otherwise returns a human readable version
+    //of feed
+    public String getPrettyFeed() {
+        if (doc == null) 
+            return null;
+
+        StringBuilder feedBuilder = new StringBuilder();
+        Element feed = doc.getDocumentElement();
+        return "";    
     }
 }
--- src/test/java/atom/AtomParserTest.java
+++ src/test/java/atom/AtomParserTest.java
@@ -12,6 +12,8 @@ import org.junit.Test;
 
 /**
  * AtomParserTest
+ * Mainly tests for checking valid
+ * Atom. Involves a lot of Exception checking
  */
 public class AtomParserTest {
 
@@ -45,6 +47,97 @@ public class AtomParserTest {
     @Test(expected = InvalidAtomException.class) 
     public void throwsExceptionOnDuplicateFeedTitle() throws IOException, SAXException, InvalidAtomException {
         AtomParser parser = new AtomParser(readFile("duplicate_title.xml"));
+        parser.parseAtom();
+    }
+
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnMissingFeedId() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("no_id.xml"));
+        parser.parseAtom();
+    }
+
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnDuplicateFeedId() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("duplicate_id.xml"));
+        parser.parseAtom();
+    }
+
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnMissingFeedUpdated() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("no_updated.xml"));
+        parser.parseAtom();
+    }
+
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnDuplicateFeedUpdated() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("duplicate_updated.xml"));
+        parser.parseAtom();
+    }
+
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnDuplicateFeedSubtitle() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("duplicate_subtitle.xml"));
+        parser.parseAtom();
+    }
+
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnUnkownElement() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("unknown_element.xml"));
+        parser.parseAtom();
+    }
+
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnAuthorWithoutName() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("author_no_name.xml"));
+        parser.parseAtom();
+    }
+
+    // ***Entry Testing***    
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnEntryWithoutTitle() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("entry_no_title.xml"));
+        parser.parseAtom();
+    }
+
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnEntryDuplicateTitle() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("entry_duplicate_title.xml"));
+        parser.parseAtom();
+    }
+
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnEntryWithoutId() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("entry_no_id.xml"));
+        parser.parseAtom();
+    }
+
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnEntryDuplicateId() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("entry_duplicate_id.xml"));
+        parser.parseAtom();
+    }
+
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnEntryWithoutUpdated() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("entry_no_updated.xml"));
+        parser.parseAtom();
+    }
+
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnEntryDuplicateUpdated() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("entry_duplicate_updated.xml"));
+        parser.parseAtom();
+    }
+
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnEntryDuplicateSummary() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("entry_duplicate_summary.xml"));
+        parser.parseAtom();
+    }
+
+    @Test(expected = InvalidAtomException.class) 
+    public void throwsExceptionOnEntryNoAuthorIfFeedNoAuthor() throws IOException, SAXException, InvalidAtomException {
+        AtomParser parser = new AtomParser(readFile("entry_no_author.xml"));
         parser.parseAtom();
     }
     
